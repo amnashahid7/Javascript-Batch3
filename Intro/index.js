a171c7013b87c6c24f02532add41736031e6571f
codieamna <amnashahid@codiea.io>
2025-10-15 17:51:20 +0500

    block scope and variables

--- Intro/index.js
+++ Intro/index.js
@@ -1,1 +1,42 @@
-console.log("heelo")
+// variables
+
+// let 
+// var 
+// const
+
+
+// const pie=3.14;
+// console.log(pie);
+
+// var pie=3.1456788;
+// console.log(pie);
+// // function calculate pie/2;
+// var pie=333.33;
+// console.log(pie);
+
+// function calculate pie*2;
+
+
+// let age= 20;
+// console.log(age, "=let age ");
+// let age= 30;
+// console.log(age, "=updated age ");
+
+
+
+let name = "saif"
+console.log(name, "saif first");
+
+{
+    // let name= "saad";
+    console.log(name, "saad from block");
+
+}
+ console.log(name, "yousaf last");
+
+
+
+
+
+
+
