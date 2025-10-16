17fd2a4f0ad11e541332c18a1a7d4b857c52dbb5
codieamna <amnashahid@codiea.io>
2025-10-16 17:44:14 +0500

    datatypes

---
+++ Intro/DataType&Conversions.js
@@ -0,0 +1,32 @@
+// saif=12;
+let name = "saif";
+let age= 20;
+console.log(name);
+
+console.log("my name is name");
+// string interpolation
+console.log(`my name is ${name} and my age is ${age}`);
+
+
+// Number
+// String
+// bigint
+// boolean
+// undefined
+// Symbol
+// Object
+// null
+
+
+
+let number;
+console.log(number, typeof number);
+
+let user = {
+    name:"saif",
+    age:20
+}
+console.log(user.name, typeof user);
+
+
+
