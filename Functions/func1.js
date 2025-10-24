c2572674be50f7c58e9b7bf1664461e718043138
codieamna <amnashahid@codiea.io>
2025-10-24 17:27:29 +0500

    function start

---
+++ Functions/func1.js
@@ -0,0 +1,63 @@
+// console.log("Y");
+// console.log("o");
+// console.log("u");
+// console.log("s");
+// console.log("a");
+// console.log("f");
+
+
+
+function sayMyName(){
+console.log("Y");
+console.log("o");
+console.log("u");
+console.log("s");
+console.log("a");
+console.log("f");
+
+}
+
+// sayMyName();
+
+
+
+function addNum(num1, num2,num3){
+    console.log(num1+num2+num3);
+}
+
+// addNum(3,6,1);
+
+
+function addMe(num,num2){
+let result= num+num2;
+console.log("yousaf");
+
+    // return result
+    return num+num2
+};
+
+// const result= addMe(1,2);
+// console.log(result);
+
+// 
+
+
+function userLoggedIn(username){
+
+    if(!username){
+         console.log("please give the username");
+          
+          return;
+    }
+    else{
+       console.log(`${username} logged in`);
+        
+    }
+  
+
+    
+}
+
+userLoggedIn();
+
+
