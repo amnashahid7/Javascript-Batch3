596dec50e4d112166cfce60b29c350def5b691b2
codieamna <amnashahid@codiea.io>
2025-11-04 17:40:25 +0500

    for loop, while, do while

---
+++ Loops/forloop-while.js
@@ -0,0 +1,67 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+// for(let i=0; i<=10; i++){
+//     i= i*10
+//     console.log(i);}
+
+
+// let myArray=[1,2,3,4,556,6,66655,33,44];
+// for (let index = 0; index < myArray.length; index++) {
+//    console.log(myArray[index]);
+   
+    
+// }
+
+
+
+// for(let i=1;i<=3;i++){
+//     for(j=1; j<=100; j++){
+//         console.log(` ${i} * ${j} = ` + i*j);
+        
+//     }
+// }
+
+
+
+
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+//         // console.log("number is 5");
+//         break
+//     }
+//     console.log(i);
+    
+// }
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+//         //console.log("number is 5");
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+
+//while loop
+// let index=1;
+// while (index<=10) {
+//     console.log(index, "heel");
+//     index= index+2
+// }
+
+
+// do while loop
+let i="heeelo";
+do {
+    console.log(i);
+    i++;
+} while (i<10);
+
+
+// do {
+    
+// } while (condition);
