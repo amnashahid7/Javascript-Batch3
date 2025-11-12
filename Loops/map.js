262b066b44f0e1c260a926301497a817066d87dd
codieamna <amnashahid@codiea.io>
2025-11-12 17:26:40 +0500

    filter, map

---
+++ Loops/map.js
@@ -0,0 +1,14 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// myNumers.map((x)=>{console.log(x*10);
+// });
+// const values= myNumers.map((x)=>{return x+1});
+// console.log(values);
+
+
+
+const calcualte= myNumers
+                 .map(x=> x*2)
+                 .map(x=>x+1)
+console.log(calcualte);
+                 
