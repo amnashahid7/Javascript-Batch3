d99ffec4e8b7a8aae6231f22dc28ccc4dc26fd2f
codieamna <amnashahid@codiea.io>
2025-12-03 17:34:35 +0500

    setInterval , guess game initial

---
+++ Projects/guessGame/index.js
@@ -0,0 +1,43 @@
+let randomNumber =Math.floor(Math.random()*100 +1);
+// console.log(randomNumber);
+
+let userInput= document.getElementById('guessField');
+let submitBtn= document.getElementById('subt');
+let guessArray= document.getElementById('guesses');
+let attempts= document.getElementById('lastResult');
+let lowhi= document.getElementById('lowOrHi');
+
+let p = document.createElement('p');
+
+
+let playGame= true;
+if(playGame){
+    
+}
+else{
+    endGame();
+}
+
+function validateguess(guess){
+    //
+}
+function checkGuess(guess){
+    //
+}
+
+function displayMessage(message){
+
+}
+
+function displayGuesses(guess){
+
+}
+
+function endGame(){
+    //
+}
+
+function startGame(){
+    //
+}
+
