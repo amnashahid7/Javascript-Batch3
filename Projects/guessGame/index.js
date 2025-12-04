d491cfa50731715cab518534ae5e22c9c4fbb04f
codieamna <amnashahid@codiea.io>
2025-12-04 17:31:23 +0500

    guess game

--- Projects/guessGame/index.js
+++ Projects/guessGame/index.js
@@ -1,43 +1,83 @@
-let randomNumber =Math.floor(Math.random()*100 +1);
+let randomNumber = Math.floor(Math.random() * 100 + 1);
 // console.log(randomNumber);
 
-let userInput= document.getElementById('guessField');
-let submitBtn= document.getElementById('subt');
-let guessArray= document.getElementById('guesses');
-let attempts= document.getElementById('lastResult');
-let lowhi= document.getElementById('lowOrHi');
+let userInput = document.getElementById("guessField");
+let submitBtn = document.getElementById("subt");
+let guessArray = document.querySelector(".guesses");
+let attempts = document.querySelector(".lastResult");
+let lowhi = document.querySelector(".lowOrHi");
 
-let p = document.createElement('p');
+let p = document.createElement("p");
 
+let playGame = true;
+let numGuess = 0;
+let prevArray = [];
+if (playGame) {
+  submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let guess = parseInt(userInput.value);
+    console.log(guess);
+    
+    validateguess(guess);
+  });
+}
 
-let playGame= true;
-if(playGame){
+function validateguess(guess) {
+    if(isNaN(guess)){
+        displayMessage("Please enter a valid number");
+    }
+    else if(guess < 1){
+        displayMessage("Please enter a number greater than 0");
+    }
+    else if(guess > 100){
+        displayMessage("Please enter a number less than 100");
+    }else{
+        prevArray.push(guess);
+        if(numGuess=== 10){
+            displayGuesses(guess);
+            displayMessage(`Game over and random number is ${randomNumber}`);
+            endGame();
+        }
+        else{
+            displayGuesses(guess);
+            checkGuess(guess);
+        }
+    }
+  //
+}
+function checkGuess(guess) {
+    if(guess==randomNumber){
+        displayMessage("You Win");
+        endGame();
+    }else if(guess<randomNumber){
+        displayMessage("your guess is tooo low")
+    }else if(guess>randomNumber){
+        displayMessage("your guess is too high")
+    }
+}
+function displayGuesses() {
+    userInput.value= "";
+
+    guessArray.innerHTML= `${prevArray}`;
+    
+    numGuess++;
+    console.log(numGuess," numguess");
+    
+    attempts.innerHTML = `${10-numGuess}`;
+    console.log(attempts, " attempts");
+    
     
 }
-else{
-    endGame();
+function displayMessage(message) {
+    lowhi.innerHTML= `${message}`
 }
 
-function validateguess(guess){
-    //
-}
-function checkGuess(guess){
-    //
-}
 
-function displayMessage(message){
 
+function endGame() {
+  //
 }
 
-function displayGuesses(guess){
-
+function startGame() {
+  //
 }
-
-function endGame(){
-    //
-}
-
-function startGame(){
-    //
-}
-
